0407e65bea041a90505a00cfdb32085bf5ae83f7
fifixc <122852085+Fifixex@users.noreply.github.com>
2024-06-30 03:16:36 +0000

    Initial commit

---
+++ src/lib/utils.ts
@@ -0,0 +1,18 @@
+export async function createLogin(login: string, password: string) {
+  const response = await fetch('https://discord.com/api/v9/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      login,
+      password,
+      undelete: false,
+      login_source: null,
+      gift_code_sku_id: null
+    })
+  })
+  const data  = await response.json()
+  
+  return data
+}
---
+++ src/routes/login.ts
@@ -0,0 +1,11 @@
+import { Hono } from 'hono'
+import { createLogin } from '../lib/utils'
+
+export const loginRoute = new Hono()
+
+loginRoute.post('/login', async (c) => {
+  let { login, password } = await c.req.json()
+
+  let response = await createLogin(login, password)
+  return c.json(response)
+})
