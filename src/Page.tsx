a172799d5bf44fdb6e692952078abe366ed7d5ee
fifixc <122852085+Fifixex@users.noreply.github.com>
2024-06-30 05:38:57 +0000

    feat: add Layout component

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,10 +1,15 @@
 import { css } from "hono/css";
 import type { FC } from "hono/jsx";
+import { Layout } from "./Layout";
 
 const pClass = css`
   color: red;
 `;
 
 export const Page: FC = () => {
-  return <p class={pClass}>Hola que tal</p>;
+  return (
+    <Layout>
+      <p class={pClass}>Hola que tal</p>
+    </Layout>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import { Page } from "./Page";
 
 const app = new Hono().basePath("/api/v9");
 
-app.get("/", (c) => {
-  return c.html(<Page />);
-});
+app.get("/", (c) =>  c.html(<Page />));
 
 export { app };
