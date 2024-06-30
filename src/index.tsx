fba0892c7413cfc0c5b5706634e6e1b93c070af7
fifixc <122852085+Fifixex@users.noreply.github.com>
2024-06-30 05:30:41 +0000

    test: remove mjs module type

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,10 @@
-// import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-
-import { BASE_PATH, PORT } from "./config";
-
-import { cors } from "hono/cors";
-import { logger } from "hono/logger";
-import { prettyJSON } from "hono/pretty-json";
-
-// import { loginRoute } from "./routes";
-
-const app = new Hono().basePath(BASE_PATH);
-
-app.use("*", prettyJSON(), logger());
-app.use(
-  "*",
-  cors({
-    origin: "*",
-    allowMethods: ["GET", "POST", "OPTIONS"],
-  }),
-);
-
-// app.route("/auth", loginRoute);
-
 import { Page } from "./Page";
+
+const app = new Hono().basePath("/api/v9");
 
 app.get("/", (c) => {
   return c.html(<Page />);
 });
-
-console.log(`Server is running on port ${PORT}`);
-
-// serve({
-//   fetch: app.fetch,
-//   port: +(process.env.PORT ?? PORT),
-// });
 
 export { app };
