a076231a6998505628643aa06493b927a3839d92
Fifix <122852085+Fifixex@users.noreply.github.com>
2024-07-06 23:37:11 -0300

    feat(web): add postgresql database

---
+++ apps/web/src/lib/database.ts
@@ -0,0 +1,22 @@
+import {neon} from '@neondatabase/serverless'
+
+export const sql = neon(process.env.DATABASE_URL ?? '')
+
+sql(`CREATE TABLE IF NOT EXISTS user (
+    id TEXT NOT NULL PRIMARY KEY,
+    discord_id INTEGER NOT NULL UNIQUE,
+    username TEXT NOT NULL
+)`)
+
+sql(`CREATE TABLE IF NOT EXISTS session (
+    id TEXT NOT NULL PRIMARY KEY,
+    expires_at INTEGER NOT NULL,
+    user_id TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+)`)
+
+export interface DatabaseUser {
+  id: string
+  username: string
+  discord_id: number
+}
